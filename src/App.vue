9611be6fe022df30c0c2e9de90faa9f93b18b263
Sasha Kramer <sashasemail@gmail.com>
2020-08-15 16:10:01 -0400

    add title

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <div id="app">
-    <!-- <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <Title />
     <Cocktails />
   </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
+import Title from './components/Title.vue'
 import Cocktails from './components/Cocktails.vue'
 
 export default {
   name: 'App',
   components: {
-    Cocktails
+    Cocktails,
+    Title
   }
 }
 </script>
---
+++ src/components/Title.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="title">
+        <div class="title-text">
+            Crump Cocktails
+        </div>            
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+.title {
+    font-size: 35px;
+    
+    text-align: center;
+    margin-bottom: 50px;
+}
+.title-text {
+    display: inline-block;
+    border-bottom: solid 1px black;
+}
+</style>
