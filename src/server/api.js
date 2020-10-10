9dff2ade8ef9f91e48303ff449df76f5a38a4fe5
Sasha Kramer <sashasemail@gmail.com>
2020-10-10 15:55:24 -0400

    add some config

--- src/server/api.js
+++ src/server/api.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8000
 var db = require('./db')
 
 app.get('/cocktails', async (req, res) => {
