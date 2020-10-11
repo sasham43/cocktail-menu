706cacc258869651ec284c0148c85ad700441609
Sasha Kramer <sashasemail@gmail.com>
2020-10-11 12:04:50 -0400

    don't end query

--- src/server/db.js
+++ src/server/db.js
@@ -14,7 +14,7 @@ const db = new Client({
         await db.connect()
         const res = await db.query('SELECT $1::text as message', ['Hello world!'])
         console.log(res.rows[0].message) // Hello world!
-        await db.end()
+        // await db.end()
     })()
 
 
