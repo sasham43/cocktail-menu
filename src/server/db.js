e9e7a56a320e740f064116c349814c94df31c809
Sasha Kramer <sashasemail@gmail.com>
2020-10-11 12:26:56 -0400

    remove sqlite code

--- src/server/db.js
+++ src/server/db.js
@@ -104,13 +104,13 @@ async function addIngredient(cocktail_id, ingredient){
         await queryDatabase('INSERT INTO ingredients (cocktail_id, stock_id, parts) VALUES ($1, $2, $3);', [cocktail_id, stock[0].id, ingredient.parts])
     } else {
         // insert stock
-        await queryDatabase('INSERT INTO stock (type, in_stock) VALUES ($1, false);', [ingredient.name])
-        var new_stock = await queryDatabase('SELECT last_insert_rowid();')
+        var new_stock = await queryDatabase('INSERT INTO stock (type, in_stock) VALUES ($1, false) RETURNING id;', [ingredient.name])
+        // var new_stock = await queryDatabase('SELECT last_insert_rowid();')
         console.log('inserted new stock', new_stock)
-        if (new_stock && new_stock.length > 0 && new_stock[0]['last_insert_rowid()'] != undefined) {
-            var new_stock_id = new_stock[0]['last_insert_rowid()']
+        // if (new_stock && new_stock.length > 0 && new_stock[0]['last_insert_rowid()'] != undefined) {
+            var new_stock_id = new_stock.rows[0]['id()']
             await queryDatabase('INSERT INTO ingredients (cocktail_id, stock_id, parts) VALUES ($1, $2, $3);', [cocktail_id, new_stock_id, ingredient.parts])
-        }
+        // }
     }
 
 }
