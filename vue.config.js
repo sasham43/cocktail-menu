41c357fad0ab544cb670a79b18d8a032e5483153
Sasha Kramer <sashasemail@gmail.com>
2020-10-12 17:24:18 -0400

    change title

--- vue.config.js
+++ vue.config.js
@@ -5,3 +5,13 @@
 //         before: configureAPI
 //     }
 // }
+module.exports = {
+    chainWebpack: config => {
+        config
+            .plugin('html')
+            .tap(args => {
+                args[0].title = 'Crump Cocktails'
+                return args
+            })
+    }
+}
